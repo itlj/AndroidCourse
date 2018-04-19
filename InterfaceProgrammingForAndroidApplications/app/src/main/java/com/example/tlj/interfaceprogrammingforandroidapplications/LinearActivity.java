d99598be900511b9aa059f3b7acd8081d87a07d2
TLJ <492542473@qq.com>
2018-04-19 19:46:24 +0800

    添加了LinearLayout三个按钮的功能

--- InterfaceProgrammingForAndroidApplications/app/src/main/java/com/example/tlj/interfaceprogrammingforandroidapplications/LinearActivity.java
+++ InterfaceProgrammingForAndroidApplications/app/src/main/java/com/example/tlj/interfaceprogrammingforandroidapplications/LinearActivity.java
@@ -3,8 +3,10 @@ package com.example.tlj.interfaceprogrammingforandroidapplications;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 /**
  * Data : 2018/4/16
@@ -19,12 +21,15 @@ public class LinearActivity extends Activity {
     private Button bn_horizontal;
     private Button bn_vertical;
     private Button bn_left;
+    private LinearLayout layout ;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.linear);
 
+
+        layout = (LinearLayout)findViewById(R.id.line_layout);
         bn_back = (Button)findViewById(R.id.line_bn_back);
         bn_horizontal = (Button)findViewById(R.id.bn_horizontal);
         bn_vertical = (Button)findViewById(R.id.bn_vertical);
@@ -35,6 +40,27 @@ public class LinearActivity extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(LinearActivity.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        bn_horizontal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setOrientation(LinearLayout.HORIZONTAL);
+            }
+        });
+
+        bn_vertical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setOrientation(LinearLayout.VERTICAL);
+            }
+        });
+
+        bn_left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.setGravity(Gravity.LEFT);
             }
         });
 
