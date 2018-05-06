b5c2b88cc8e88733941397ea105d14cdc5a61a5d
TLJ <492542473@qq.com>
2018-05-06 18:48:29 +0800

    实验三第二小题

---
+++ Experiment32/app/src/main/java/cn/edu/hqu/cst/tlj/experiment3_2/MyButton.java
@@ -0,0 +1,27 @@
+package cn.edu.hqu.cst.tlj.experiment3_2;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Button;
+
+/**
+ * Data : 2018/5/6
+ *
+ * @author : TLJ
+ * @parameter :
+ * @return :
+ */
+public class MyButton extends android.support.v7.widget.AppCompatButton {
+    public MyButton(Context context , AttributeSet set){
+            super(context , set);
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        super.onKeyDown(keyCode , event);
+        Log.v("com.sise" , "the onKeyDown in MyButton");
+        //返回true，表明该事件不会向外扩散
+        return true;
+    }
+}
